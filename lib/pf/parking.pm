60ef0b13d673c947e4078e8cb3d8b2fb9a6c927e
Durand Fabrice <fdurand@inverse.ca>
2016-05-27 11:50:56 -0400

    Added parking workflow

--- lib/pf/parking.pm
+++ lib/pf/parking.pm
@@ -16,12 +16,12 @@ use strict;
 use warnings;
 
 use pf::log;
-use pf::OMAPI;
 use pf::violation;
 use pf::constants::parking qw($PARKING_VID $PARKING_DHCP_GROUP_NAME $PARKING_IPSET_NAME);
 use pf::constants;
 use pf::config qw(%Config);
 use pf::util;
+use pf::dhcpd;
 
 =head2 trigger_parking
 
@@ -48,8 +48,7 @@ sub park {
     my ($mac,$ip) = @_;
     get_logger->debug("Setting client in parking");
     if(isenabled($Config{parking}{place_in_dhcp_parking_group})){
-        my $omapi = pf::OMAPI->get_client();
-        $omapi->create_host($mac, {group => $PARKING_DHCP_GROUP_NAME});
+        freeradius_update_dhcpd_lease($mac, $Config{'parking'}{'lease_length'});
     }
     if(isenabled($Config{parking}{show_parking_portal})){
         my $cmd = "sudo ipset add $PARKING_IPSET_NAME $ip 2>&1";
@@ -86,8 +85,7 @@ Remove the parking actions that were taken against an IP + MAC
 sub remove_parking_actions {
     my ($mac, $ip) = @_;
     get_logger->info("Removing parking actions for $mac - $ip");
-    my $omapi = pf::OMAPI->get_client();
-    $omapi->delete_host($mac);
+    freeradius_delete_dhcpd_lease($mac);
 
     pf_run("sudo ipset del $PARKING_IPSET_NAME $ip -exist 2>&1");
 }
